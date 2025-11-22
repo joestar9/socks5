555a3c1ae32ada9755e581913a2a5dc2d3f85a9e
joestar9 <159675199+joestar9@users.noreply.github.com>
2025-11-22 02:12:46 +0100

    Remove sysctl configuration and related commands
    Removed extensive sysctl configuration settings and related commands from optimizer.sh.

--- optimizer.sh
+++ optimizer.sh
@@ -74,91 +74,6 @@ echo "ZRAM enabled:"
 zramctl
 swapon --show
 
-####################
-rm -f /etc/sysctl.d/99-sysctl.conf
-cat >'/etc/sysctl.d/99-sysctl.conf' <<EOF
-net.ipv4.tcp_fack = 1
-net.ipv4.tcp_early_retrans = 3
-net.ipv4.neigh.default.unres_qlen=10000  
-net.ipv4.conf.all.route_localnet=1
-net.ipv4.ip_forward = 1
-net.ipv4.conf.all.forwarding = 1
-net.ipv4.conf.default.forwarding = 1
-#net.ipv6.conf.all.forwarding = 1  #awsipv6问题
-net.ipv6.conf.default.forwarding = 1
-net.ipv6.conf.lo.forwarding = 1
-net.ipv6.conf.all.disable_ipv6 = 0
-net.ipv6.conf.default.disable_ipv6 = 0
-net.ipv6.conf.lo.disable_ipv6 = 0
-net.ipv6.conf.all.accept_ra = 2
-net.ipv6.conf.default.accept_ra = 2
-net.core.netdev_max_backlog = 100000
-net.core.netdev_budget = 50000
-net.core.netdev_budget_usecs = 5000
-#fs.file-max = 51200
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-net.core.rmem_default = 67108864
-net.core.wmem_default = 67108864
-net.core.optmem_max = 65536
-net.core.somaxconn = 1000000
-net.ipv4.icmp_echo_ignore_all = 1
-net.ipv4.icmp_echo_ignore_broadcasts = 1
-net.ipv4.icmp_ignore_bogus_error_responses = 1
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv4.conf.default.accept_redirects = 0
-net.ipv4.conf.all.secure_redirects = 0
-net.ipv4.conf.default.secure_redirects = 0
-net.ipv4.conf.all.send_redirects = 0
-net.ipv4.conf.default.send_redirects = 0
-net.ipv4.conf.default.rp_filter = 0
-net.ipv4.conf.all.rp_filter = 0
-net.ipv4.tcp_keepalive_time = 600
-net.ipv4.tcp_keepalive_intvl = 15
-net.ipv4.tcp_keepalive_probes = 2
-net.ipv4.tcp_synack_retries = 1
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_rfc1337 = 0
-net.ipv4.tcp_timestamps = 1
-net.ipv4.tcp_tw_reuse = 0
-net.ipv4.tcp_fin_timeout = 15
-net.ipv4.ip_local_port_range = 1024 65535
-net.ipv4.tcp_max_tw_buckets = 5000
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
-net.ipv4.udp_rmem_min = 8192
-net.ipv4.udp_wmem_min = 8192
-net.ipv4.tcp_mtu_probing = 1
-net.ipv4.tcp_autocorking = 0
-net.ipv4.tcp_slow_start_after_idle = 0
-net.ipv4.tcp_max_syn_backlog = 819200
-net.ipv4.tcp_notsent_lowat = 16384
-net.ipv4.tcp_no_metrics_save = 0
-net.ipv4.tcp_ecn = 1
-net.ipv4.tcp_ecn_fallback = 1
-net.ipv4.tcp_frto = 0
-net.ipv6.conf.all.accept_redirects = 0
-net.ipv6.conf.default.accept_redirects = 0
-net.ipv4.neigh.default.gc_thresh3=8192
-net.ipv4.neigh.default.gc_thresh2=4096
-net.ipv4.neigh.default.gc_thresh1=2048
-net.ipv6.neigh.default.gc_thresh3=8192
-net.ipv6.neigh.default.gc_thresh2=4096
-net.ipv6.neigh.default.gc_thresh1=2048
-net.ipv4.tcp_orphan_retries = 1
-net.ipv4.tcp_retries2 = 5
-vm.swappiness = 1
-vm.overcommit_memory = 1
-kernel.pid_max=64000
-net.netfilter.nf_conntrack_max = 262144
-net.nf_conntrack_max = 262144
-## Enable bbr
-net.core.default_qdisc = cake
-net.ipv4.tcp_congestion_control = bbr
-net.ipv4.tcp_low_latency = 1
-EOF
-
 rm -f /etc/sysctl.conf
 cat <<EOF >/etc/sysctl.conf
 net.ipv4.tcp_fack = 1
@@ -276,19 +191,6 @@ root     soft   memlock   unlimited
 *     hard   memlock   unlimited
 *     soft   memlock   unlimited
 EOF
-
-sed -i '/ulimit -SHn/d' /etc/profile
-sed -i '/ulimit -SHu/d' /etc/profile
-echo "ulimit -SHn 1000000" >>/etc/profile
-
-if grep -q "pam_limits.so" /etc/pam.d/common-session; then
-	:
-else
-	sed -i '/required pam_limits.so/d' /etc/pam.d/common-session
-	echo "session required pam_limits.so" >>/etc/pam.d/common-session
-fi
-systemctl daemon-reload
-echo -e "${Info} Optimization has finished applying and may require a restart!"
 
 #######################
 
